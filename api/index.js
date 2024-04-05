4328b8a461678566a1bf4da880c3e79b48ef4dd3
saikirankonda <saikiran@rsaamerica.com>
2024-04-05 21:17:01 +0530

    cors  policy error issue fixed

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
@@ -12,6 +13,8 @@ mongoose
   .catch((error) => console.log(error));
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 
 const port = 3000;
@@ -23,9 +26,9 @@ app.use("/api/auth", authRouter);
 app.use((err, req, res, next) => {
   const statuscode = err.statuscode || 500;
   const message = err.statusmessage || "Internal Server Error";
-    return res.status(statuscode).json({
-      success: false,
-      statuscode,
-      message
+  return res.status(statuscode).json({
+    success: false,
+    statuscode,
+    message
   });
 });
